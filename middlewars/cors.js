66725f38989012c786e7dd151029136c9a5191c2
Всем привет, меня зовут Роман! <86961006+iEasyJet@users.noreply.github.com>
2023-01-19 12:27:57 +0300

    Update cors

--- middlewars/cors.js
+++ middlewars/cors.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsHandler = cors({
-  origin: 'localhost:3003, http://diploma.easyjet.nomoredomains.work, https://diploma.easyjet.nomoredomains.work',
+  origin: 'localhost:3003, https://ieasyjet.github.io/movies-explorer-frontend, http://ieasyjet.github.io/movies-explorer-frontend',
   methods: ['OPTIONS', 'GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   allowedHeaders: ['Content-Type', 'origin', 'Authorization', 'Cookie'],
   credentials: true,
